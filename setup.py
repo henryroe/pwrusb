c8c3ce6c06c3e2ce4a18cf87886d875a86d70337
Henry Roe <hroe@hroe.me>
2014-10-31 11:17:11 -0700

    continued cleanup toward enabling pypi distro

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
-from setuptools import setup
+# from setuptools import setup
+from distutils.core import setup
 from distutils.command.install import install
 from distutils.command.build import build
 from distutils.sysconfig import get_python_lib
@@ -8,15 +9,18 @@ import pdb
 
 class MyBuild(build):
     def run(self):
-        subprocess.call("make")
+        subprocess.call("make _pwrusb.so")
         build.run(self)
 
 setup(name="pwrusb",
       version="0.2.0",
+      packages=['pwrusb'],
       description="Control pwrusb.com power strip outlets",
       long_description=open('README.md').read(),
       author='Henry Roe',
       author_email='hroe@hroe.me',
+      url='https://github.com/henryroe/pwrusb',
+      download_url='https://github.com/henryroe/pwrusb/tarball/0.2.0',
       license="LICENSE.TXT",
       cmdclass={'build': MyBuild},
       data_files=[(get_python_lib() + '/', ['_pwrusb.so']),
